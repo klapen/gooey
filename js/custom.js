1192429b90e8c325d5e4d2e0b08131cc3630f7d9
klapen <oasaavedrac@gmail.com>
2016-07-14 22:12:20 -0500

    Transform circular animation to linear

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,6 @@
 var oas;
 
-// Code taken from http://bl.ocks.org/nbremer/69808ec7ec07542ed7df
-function gooeyEffect(){
-    var w = window,
-	d = document,
-	e = d.documentElement,
-	g = d.getElementsByTagName('body')[0],
-	x = w.innerWidth || e.clientWidth || g.clientWidth,
-	totalHeight = w.innerHeight || e.clientHeight || g.clientHeight;
-    
-    var margin = {top: 30, right: 30, bottom: 30, left: 30};
-    width = Math.min(500, (totalHeight - 20), $("#chart").width() - margin.left - margin.right);
-    height = width;
-    
-    //Create scale
-    var xScale = d3.scale.linear()
-	.domain([-1.5, 1.5])
-	.range([-width/2, width/2]);
-    
-    //Create SVG
-    var svg = d3.select("#chart").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.style("filter", "url(#gooey)") //Set the filter on the container svg
-	.attr("transform", "translate(" + (width/2 + margin.left) + "," +(height/2 +  margin.top) + ")");
-
+function createGooeyFilter(svg){
     //SVG filter for the gooey effect
     //Code taken from http://tympanus.net/codrops/2015/03/10/creative-gooey-effects/
     var defs = svg.append('defs');
@@ -42,7 +17,38 @@ function gooeyEffect(){
     filter.append('feComposite')
 	.attr('in','SourceGraphic')
 	.attr('in2','gooey')
-	.attr('operator','atop');	
+	.attr('operator','atop');
+}
+
+// Code taken from http://bl.ocks.org/nbremer/69808ec7ec07542ed7df
+function gooeyEffect(){
+    var color = "steelblue";
+    var w = window,
+	d = document,
+	e = d.documentElement,
+	g = d.getElementsByTagName('body')[0],
+	x = w.innerWidth || e.clientWidth || g.clientWidth,
+	totalHeight = w.innerHeight || e.clientHeight || g.clientHeight;
+    
+    var margin = {top: 30, right: 30, bottom: 30, left: 30};
+    width = Math.min(500, (totalHeight - 20), $("#chart").width() - margin.left - margin.right);
+    height = width;
+    
+    //Create scale
+    var steps = 5;
+    var xScale = d3.scale.linear()
+	.domain([0, steps])
+	.range([-width/2, width/2]);
+    
+    //Create SVG
+    var svg = d3.select("#chart").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.style("filter", "url(#gooey)") //Set the filter on the container svg
+	.attr("transform", "translate(" + (width/2 + margin.left) + "," +margin.top + ")");
+
+    createGooeyFilter(svg);
     
     //Append circle at center
     svg.append("circle")
@@ -50,31 +56,34 @@ function gooeyEffect(){
 	.attr("cx", 0)
 	.attr("cy", 0)
 	.attr("r", 20)
-	.style("fill", "#81BC00");
+	.style("fill",color);
     
     //Create the circles that will move out and in the center circle
-    var steps = 15;	
     svg.selectAll(".flyCircle")
-	.data(d3.range(steps).map(function(num) {return (num/steps)*(2*Math.PI); }))
+	.data(d3.range(steps).map(function(num) {
+	    // Change to a line
+	    return num;
+	    //return (num/steps)*(2*Math.PI);
+	}))
 	.enter().append("circle")
 	.attr("class", "flyCircle")
 	.attr("cx", 0)
 	.attr("cy", 0)
 	.attr("r", 15)
-	.style("fill", "#81BC00")
+	.style("fill", color)
 	.call(update);
 
     //Continuously moves the circles outward and inward
     function update() {
+	var chart_width = $('#chart svg').width()-margin.left;
 	var circle = d3.selectAll(".flyCircle");
-	var dur = 1500,
-	    del = 500;
+	var dur = 1500,del = 500;
 	
 	(function repeat() {
 	    circle
 		.transition("outward").duration(dur).delay(function(d,i) { return i*del; })
-		.attr("cy", function(d) { return xScale(Math.sin(d)); })
-		.attr("cx", function(d) { return xScale(Math.cos(d)); })
+		.attr("cy", function(d) { return $('#chart svg').height()/4;})
+		.attr("cx", function(d) { return xScale(d); })
 		.transition("inward").duration(dur).delay(function(d,i) { return steps*del + i*del; })
 		.attr("cx", 0)
 		.attr("cy", 0)
@@ -86,9 +95,9 @@ function gooeyEffect(){
     //Calls a function only after the total transition ends
     function endall(transition, callback) { 
 	var n = 0; 
-	transition 
-	    .each(function() { ++n; }) 
-		.each("end", function() { if (!--n) callback.apply(this, arguments); }); 
+	transition
+	    .each(function(){++n;}) 
+	    .each("end", function(){if(!--n) callback.apply(this,arguments);}); 
     }//endall
 }
 
